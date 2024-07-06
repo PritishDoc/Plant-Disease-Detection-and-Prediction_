997dc7467bd552c6022810fb6d432816fff9f5c2
Pritish kumar Ray <pritishray1999@gmail.com>
2024-07-06 10:41:14 +0530

    code updated

--- App.py
+++ App.py
@@ -3,7 +3,14 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image, UnidentifiedImageError
 
-
+# TensorFlow model prediction 
+def model_prediction(image):
+    model = tf.keras.models.load_model('trained_model.keras')
+    image = image.resize((128, 128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr])  # Convert single image to a batch
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions)  # Return the index of the max element
 
 st.sidebar.title("Dashboard")
 app_mode = st.sidebar.selectbox("Select Page", ["Home", "About", "Disease Recognition"])
